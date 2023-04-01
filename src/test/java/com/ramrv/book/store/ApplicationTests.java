eaf4a3c61062fdc080b42f7db60a569448707de5
ramvalli <iamrvr21@gmail.com>
2023-04-01 11:45:11 +0530

    initial repo creation with Spring initialzr

---
+++ src/test/java/com/ramrv/book/store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramrv.book.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
